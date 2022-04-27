3f9ab85d5689edce89ef1f8910031d5b2859f3a5
Stud 1 <45938852+frakiec89@users.noreply.github.com>
2022-04-27 13:37:34 +0400

    1 тест

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+
+        /// <summary>
+        ///  рассчитывать целое количество сырья, необходимого для производства определенного количества (count) продукции
+        /// </summary>
+        /// <param name="productType"> Тип продукции  </param>
+        /// <param name="materialType">Тип материала</param>
+        /// <param name="count">Кол-во</param>
+        /// <param name="width">Ширина</param>
+        /// <param name="length">Длинна</param>
+        /// <returns>целое количество необходимого сырья, -1 если не  корректные  данные </returns>
+        public int GetQuantityForProduct (int productType, int materialType, int count, float width, float length)
+        {
+            return 0;
+        }
+    }
+}
---
+++ WSUniversalLibTests/CalculationTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProductTest_114148()
+        {
+           int productType = 3;
+           int materialType = 1;
+           int count = 15;
+           float width = 20f;
+           float length = 45f;
+           int expected = 114148;
+
+           Calculation calculation = new Calculation(); 
+           var actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+           Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
